5d96bbafd8bd847f28402ac8ab0c8c7d3abd194e
j-ackamir <j.ackson@live.ca>
2024-01-20 17:40:47 -0500

    hi

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAnswer(element) {
+    var answer = element.nextElementSibling;
+
+    if (answer.style.display === 'none') {
+        answer.style.display = 'block';
+    } else {
+        answer.style.display = 'none';
+    }
+}
