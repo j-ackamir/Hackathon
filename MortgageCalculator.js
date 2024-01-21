5cf0b50cf7032c4ade615bdac55887e87c88d0a1
OviyaSinna <oviyacu@gmail.com>
2024-01-20 20:25:29 -0500

    calculator fixed

--- MortgageCalculator.js
+++ MortgageCalculator.js
@@ -11,7 +11,7 @@ function calculator(){
 
     if (mortgage < 0){
         document.getElementById("result").innerText = 'Result unavailable, try different numbers';
-    }else if (mortgage=Infinity){
+    }else if (mortgage==Infinity){
         document.getElementById("result").innerText = 'Result unavailable, try different numbers';
     } else {
           document.getElementById("result").innerText = 'The monthly payment is $' + mortgage.toFixed(2);
