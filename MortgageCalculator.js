242490e65e0177dd4831de66f3a6778bcfea88ee
OviyaSinna <oviyacu@gmail.com>
2024-01-20 22:10:33 -0500

    footer fixing

--- MortgageCalculator.js
+++ MortgageCalculator.js
@@ -34,3 +34,18 @@ function loan(){
           document.getElementById("loanResult").innerText = 'The monthly payment is $' + pmt.toFixed(2);
 }
 setInterval(loan,100);
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
